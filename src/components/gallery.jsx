c9bb80122839890a97694b977727f2d5c7bf5550
DFerranti <dferrantisc@hotmail.com>
2021-10-23 19:54:18 -0400

    first commit

---
+++ src/components/gallery.jsx
@@ -0,0 +1,27 @@
+import { Image } from "./image";
+
+export const Gallery = (props) => {
+  return (
+    <div id='portfolio' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Galeria de Fotos</h2>
+          <p>
+            Confira as fotos do que temos disponível na nossa loja
+          </p>
+        </div>
+        <div className='row'>
+          <div className='portfolio-items'>
+            {props.data
+              ? props.data.map((d, i) => (
+                <div key={`${d.title}-${i}`} className='col-sm-6 col-md-4 col-lg-4'>
+                  <Image title={d.title} largeImage={d.largeImage} smallImage={d.smallImage} />
+                </div>
+              ))
+              : 'Loading...'}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
